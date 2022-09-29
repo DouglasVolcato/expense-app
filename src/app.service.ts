78ead5f6e61efae1269923078ecabe2d8fecb222
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-29 17:14:58 -0300

    Added app services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { data, ReportType } from './data';
+import { v4 as uuid } from 'uuid';
+import { Injectable } from '@nestjs/common/decorators';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(reportType: ReportType) {
+    return data.report.filter((report) => report.type === reportType);
+  }
+
+  getReportById(reportType: ReportType, id: string) {
+    return data.report
+      .filter((report) => report.type === reportType && report.id === id)
+      .find((report) => report.id === id);
+  }
+
+  postReport(reportType: ReportType, body: { source: string; amount: number }) {
+    const newReport = {
+      id: uuid(),
+      source: body.source,
+      amount: body.amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: reportType,
+    };
+
+    data.report.push(newReport);
+
+    return newReport;
+  }
+
+  putReport(body: { source?: string; amount?: number }, id: string) {
+    const reportToUpdate = data.report.find((report) => report.id === id);
+    const update = Object.assign(reportToUpdate, body);
+
+    data.report.splice(data.report.indexOf(reportToUpdate), 1, update);
+
+    return update;
+  }
+
+  deleteReport(id: string) {
+    const reportToDelete = data.report.find((report) => report.id === id);
+
+    data.report.splice(data.report.indexOf(reportToDelete), 1);
+
+    return reportToDelete;
   }
 }
